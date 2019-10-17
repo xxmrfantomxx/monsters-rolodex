4cf7ac33a5faa83a0633bf5ae7e2080963b19b2c
Jakub <jakub014@seznam.cz>
2019-10-17 19:13:57 +0200

    Updates

---
+++ precache-manifest.e8a697483ac43d79a6584cd7551e2926.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc415357e680a3bbcfe6213a8d26d3f8",
+    "url": "/monsters/rolodex/index.html"
+  },
+  {
+    "revision": "266ed7b9cdf25cf7b6f9",
+    "url": "/monsters/rolodex/static/css/main.31ac2b53.chunk.css"
+  },
+  {
+    "revision": "0b9227135eb05e7a2f42",
+    "url": "/monsters/rolodex/static/js/2.cdb902b1.chunk.js"
+  },
+  {
+    "revision": "266ed7b9cdf25cf7b6f9",
+    "url": "/monsters/rolodex/static/js/main.2a194fff.chunk.js"
+  },
+  {
+    "revision": "12ad7a2fcbef463625e7",
+    "url": "/monsters/rolodex/static/js/runtime-main.06178c0f.js"
+  }
+]);
